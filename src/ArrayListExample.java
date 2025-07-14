d680a3ca2a4f9bb0d563a6f724f16a4a8ec40c8e
MeowTaiOuO <xiaosa4596@gmail.com>
2025-07-14 21:39:22 +0800

    v2

---
+++ src/ArrayListExample.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("蘋果");
+        list.add("香蕉");
+        System.out.println(list.get(1));
+    }
+}
